d0e40c6b7689613576762a445120d411e0a13903
Andreas Törnkvist <andreas.o.tornkvist@gmail.com>
2022-05-03 12:35:30 +0200

    CORS

--- nodeServer.js
+++ nodeServer.js
@@ -16,6 +16,14 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next();
+});
+
 app.get('/serverStatus', (req, res) => {
     res.send("Server is running");
     res.end();
